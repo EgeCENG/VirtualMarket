cc2bc28e5bd85daff27cdddd0a9643361356f7c5
Batuhan Kadıoğlu <kadioglubatuhan@hotmail.com>
2017-12-01 21:34:22 +0300

    product class has been created

---
+++ ConsoleApp/ConsoleApp/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Product
+    {
+        private string Brand { get; set; }
+        string Model {get;set; }
+        private int Quantity { get; set; }
+        private double Cost { get; set; }
+        private double Price { get; set; }
+        private string Description { get; set; }
+    }
+}
+
