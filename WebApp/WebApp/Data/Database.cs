cf64c3a8160ded4ea4456b53a340c27a98d81eb3
Batuhan Kadıoğlu <kadioglubatuhan@hotmail.com>
2017-12-05 20:35:39 +0300

    Test

---
+++ WebApp/WebApp/Data/Database.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using WebApp.Models;
+
+namespace WebApp.Data
+{
+    public class Database
+    {
+        private string path = "products.txt";
+
+        public List<Product> GetProduct()
+        {
+            string jsonText;
+            if (!File.Exists(path))
+            {
+                jsonText = File.ReadAllText(path);
+                if (jsonText != "")
+                {
+                    List<Product> products = JsonConvert.DeserializeObject<List<Product>>(jsonText);
+                    return products;
+                }
+                else
+                {
+                    Debug.WriteLine("The file is empty");
+                }
+            }
+            else
+            {
+                Debug.WriteLine("The file is not exist");
+            }
+            return null;
+        }
+
+        public void WriteProduct(List<Product> products)
+        {
+            
+        }
+        public bool Add(Product product)
+        {
+            List<Product> products = GetProduct();
+            products.Add(product);
+            return true;
+        }
+        public bool Delete(Product product)
+        {
+            return true;
+        }
+        public bool Delete(int id)
+        {
+            return true;
+        }
+    }
+}
